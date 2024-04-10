7c96b04c65cb23230a07bde1ee4beb06adbbf572
Vitaliy <theyamshikov@yandex.ru>
2024-04-10 13:21:45 +0500

    upd

--- src/resources/config/services/kernel.php
+++ src/resources/config/services/kernel.php
@@ -3,7 +3,6 @@
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Untek\Core\Instance\Libs\Resolvers\ArgumentMetadataResolver;
 use Untek\Core\Kernel\Config\CallableConfigLoader;
-use Untek\Core\Kernel\Config\FileConfigLoader;
 
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
@@ -11,12 +10,4 @@ return static function (ContainerConfigurator $configurator): void {
     $services = $configurator->services()->defaults()->public();
     $parameters = $configurator->parameters();
 
-    $services->set(FileConfigLoader::class, FileConfigLoader::class)
-        ->args(
-            [
-                service(ArgumentMetadataResolver::class),
-                service(CallableConfigLoader::class),
-            ]
-        )
-        ->public();
 };
